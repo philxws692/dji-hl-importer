a47179ff35afe80d16e7e1ad5170d4736168afb6
philxws692 <63463378+philxws692@users.noreply.github.com>
2025-12-05 15:11:46 +0100

    chore: init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 📷 dji-hl-importer
+Add the highlight markers from DJI Cameras into Davinci Resolve for faster editing.
+
+# ⬇️ Installation
+Download the script `dji-hl-importer.py` into the script location of your Davinci Resolve installation ([Guide](https://deric.github.io/DaVinciResolve-API-Docs/#using-a-script)).
+
+You will also need `exiftool` with version 13.41 or above, since this adds the feature for DJI Highlight Marker extraction. Download it from [here](https://exiftool.org/)
+
+# ⚒️ Usage
+Go into a Davinci Resolve Project and click on "Workspace" > "Scripts" > "dji-hl-importer" and voilà there you go.
+> [!TIP]
+> For easier usage you can also setup a keyboard shortcut to execute this script.
+
+# 📄 License
+This project is licensed under the GNU GPL-3.
---
+++ dji-hl-importer.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import pprint
+import subprocess
+
+resolve = app.GetResolve()
+pm = resolve.GetProjectManager()
+
+project = pm.GetCurrentProject()
+
+media_pool = project.GetMediaPool()
+
+
+def main():
+    for clip in media_pool.GetRootFolder().GetClipList():
+        type = str(clip.GetClipProperty("Type"))
+        if "Video" not in type:
+            continue
+        # Relevant attributes
+        # Clip Name
+        # File Path
+        name = clip.GetClipProperty("Clip Name")
+
+        # Check only for DJI clips
+        if not name.startswith("DJI_"):
+            continue
+
+        path = clip.GetClipProperty("File Path")
+        fps = clip.GetClipProperty("FPS")
+        print(f"Clip Name: {name}")
+        print(f"File Path: {path}")
+        print(f"Framerate: {fps}")
+
+        highlights = get_clip_highlights(path)
+
+        for hl in highlights:
+            hl_tc = float(hl)
+            hl_frame_id = get_frame_id_from_tc(hl_tc, float(fps))
+
+            # Check if Marker already exists at this frame
+            existing_markers = clip.GetMarkers()
+            if existing_markers:
+                if existing_markers.get(hl_frame_id):
+                    print(
+                        f"Marker already exists at frame {hl_frame_id} (tc: {hl_tc}s). Skipping..."
+                    )
+                    continue
+
+            success = clip.AddMarker(
+                hl_frame_id, "Yellow", f"Highlight @ {hl_tc}s", "", 1.0
+            )
+            print(
+                f"Added Highlight Marker at frame {hl_frame_id} (tc: {hl_tc}s)"
+            ) if success else print(
+                f"Failed to add Marker at frame {hl_frame_id} (tc: {hl_tc}s)"
+            )
+
+        print("")
+
+
+def get_clip_highlights(file_path: str) -> []:
+    # exiftool -G1 -s -Highlight\\* -ee
+    result = subprocess.run(
+        ["exiftool", "-G1", "-s", "-Highlight*", "-ee", file_path],
+        capture_output=True,
+        text=True,
+    )
+    # Format Text Output of exiftool which is something like "[QuickTime]     HighlightMarkers                : 3.649, 6.675"
+    text_result = result.stdout.split(":")[1].strip()
+
+    return [hl_tc.strip() for hl_tc in text_result.split(",")]
+
+
+def get_frame_id_from_tc(tc: float, fps: float) -> int:
+    return round(tc * fps)
+
+
+if __name__ == "__main__":
+    main()
